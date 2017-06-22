ff058341acf35ed65ef4d4f4027be0851ead86ec
raojay7 <879645920@qq.com>
2017-06-22 10:46:12 +0800

    测试首页跳转和根据页面名字跳转

---
+++ taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,25 @@
+package com.taotao.service.impl;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	TbItemMapper itemMapper;
+	
+
+	@Override
+	public TbItem getItemById(long itemId) { 
+		
+		TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		
+		return item;
+	}
+
+
+}

edbfd68268ff9e3ae91936a57559bf8ab1119373
raojay7 <879645920@qq.com>
2017-06-30 16:54:23 +0800

    新增商品包含规格参数

--- taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,7 +1,9 @@
 package com.taotao.controller;
 
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,23 +12,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class ItemController {
+public class ItemController
+{
 
-	@Autowired
-	private ItemService itemService;
-	
-	@RequestMapping("/item/{itemId}")
-	@ResponseBody
-	public TbItem getItemById(@PathVariable(value="itemId") Long itemId) {
-		TbItem item = itemService.getItemById(itemId);
-		return item;
-	}
+    @Autowired
+    private ItemService itemService;
 
-	@RequestMapping("/item/list")
-	@ResponseBody
-	public EasyUIDataGridResult getItemList(Integer page,Integer rows)
-	{
-		EasyUIDataGridResult result = itemService.getItemList(page, rows);
-		return result;
-	}
+    @RequestMapping("/item/{itemId}")
+    @ResponseBody
+    public TbItem getItemById(@PathVariable(value = "itemId") Long itemId)
+    {
+        TbItem item = itemService.getItemById(itemId);
+        return item;
+    }
+
+    @RequestMapping("/item/list")
+    @ResponseBody
+    public EasyUIDataGridResult getItemList(Integer page, Integer rows)
+    {
+        EasyUIDataGridResult result = itemService.getItemList(page, rows);
+        return result;
+    }
+
+    @RequestMapping("/save")
+    @ResponseBody
+    //添加一个itemParams参数接收规格参数的数据。
+    public TaotaoResult addItem(TbItem item, String desc, String itemParams)
+    {
+        TbItemDesc itemDesc = new TbItemDesc();
+        itemDesc.setItemDesc(desc);
+        TaotaoResult result = itemService.addItem(item, itemDesc, itemParams);
+        return result;
+    }
+
 }
